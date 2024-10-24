f83d436ba5d9447fb626d3ea681dc2244411567a
asadajohn <asa20007@byui.edu>
2024-10-24 05:40:16 -0600

    javas

--- scripts/review.js
+++ scripts/review.js
@@ -1,10 +1,10 @@
 const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
-  ];
+    { id:"fc-1888", name:"TCL 32″ FHD Android TV (32S5400AF)", averagerating :4.5 },
+    { id:"fc-2050", name:"Haier Thermocool 192 Litres Single Door DCool Refrigerator ", averagerating :4.7 },
+    { id:"fs-1987", name:"Qasa Solar Power Portable 148Wh", averagerating :3.5 },
+    { id:"ac-2000", name:"WIICES 500W HOME SOLAR LIGHTING SYSTEM FOR TV, LAPTOP AND LIGHTS", averagerating :3.9 },
+    { id:"jj-1969", name:"Djack Super Class Home Cinema System DJ 2202A", averagerating :5.0 }
+];
   
   // Add event listener to submit button
   window.onload = function() {
